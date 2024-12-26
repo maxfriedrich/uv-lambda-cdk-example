8d2ff249f556466157974d0d7e566cffc72eeac7
Max Friedrich <hallo@maxfriedrich.de>
2024-12-26 20:24:35 +0100

    cleanup

--- cdk/lambda2_stack.py
+++ cdk/lambda2_stack.py
@@ -1,19 +1,17 @@
 from aws_cdk import CfnOutput, Stack
 from constructs import Construct
+from python_lambda_function import PythonLambdaFunction
 
-from stacks.constructs.python_lambda_function import PythonLambdaFunction
 
-
-class Lambda1Stack(Stack):
+class Lambda2Stack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         function = PythonLambdaFunction(
             self,
-            "Lambda1",
-            package_name="demo-lambda1",
-            handler="demo_lambda1.lambda_function.lambda_handler",
+            "Lambda2",
+            package_name="demo-lambda2",
+            handler="demo_lambda2.lambda_function.lambda_handler",
         )
         url = function.add_function_url()
-
         CfnOutput(self, "LambdaFunctionUrl", value=url.url)
